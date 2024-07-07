31bf9a799f8ebc4b81cb7b9dce3dc800b5c488aa
tomster12 <thomas.burniston@gmail.com>
2024-07-07 12:04:07 +0100

    Supabase tutorial

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+// https://supabase.com/docs/guides/getting-started/tutorials/with-react
+
+import { useState, useEffect } from "react";
+import { supabase } from "./supabaseClient";
+
+import "./App.css";
+import Auth from "./components/Auth";
+import Account from "./components/Account";
+
+function App() {
+    const [session, setSession] = useState(null);
+
+    useEffect(() => {
+        supabase.auth.getSession().then(({ data: { session } }) => {
+            setSession(session);
+        });
+
+        supabase.auth.onAuthStateChange((_event, session) => {
+            setSession(session);
+        });
+    }, []);
+
+    return (
+        <div className="container" style={{ padding: "50px 0 100px 0" }}>
+            {!session && <Auth />}
+            {session && <Account key={session.user.id} session={session} />}
+        </div>
+    );
+}
+
+export default App;
