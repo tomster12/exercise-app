be4b6c360ac77b5e1f205321762049638e563b67
tomster12 <thomas.burniston@gmail.com>
2024-07-07 19:35:22 +0100

    Hardcoded layout progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,20 @@
 // https://supabase.com/docs/guides/getting-started/tutorials/with-react
 
-import { useState, useEffect } from "react";
-import { supabase } from "./supabaseClient";
-
 import "./App.css";
-import Auth from "./components/Auth";
-import Account from "./components/Account";
+import GlobalFeedView from "./components/GlobalFeedView";
 
 function App() {
-    const [session, setSession] = useState(null);
-
-    useEffect(() => {
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session);
-        });
-        supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-    }, []);
-
     return (
-        <div className="container" style={{ padding: "50px 0 100px 0" }}>
-            {!session && <Auth />}
-            {session && <Account key={session.user.id} session={session} />}
+        <div className="app">
+            <div className="app-side-bar">
+                <div className="_option" />
+                <div className="_option" />
+                <div className="_option" />
+                <div className="_option" />
+            </div>
+            <div className="app-content">
+                <GlobalFeedView />
+            </div>
         </div>
     );
 }
---
+++ src/components/GlobalFeedView.jsx
@@ -0,0 +1,170 @@
+import "./GlobalFeedView.css";
+import bicepImg from "../assets/biceps.png";
+import legsImg from "../assets/legs.png";
+import backImg from "../assets/back.png";
+
+function GlobalFeedView() {
+    return (
+        <div className="global-feed-view">
+            <div className="exercise-feed">
+                <div className="_day">
+                    <h1>Sunday, 11th March</h1>
+                    <hr />
+                    <div className="_exercise">
+                        <span className="_type">Rowing</span>
+                        <div className="_muscle _legs">
+                            <img alt="" />
+                            <span>Legs</span>
+                        </div>
+                        <div className="_measurement">
+                            <span>860 </span>
+                            <span>metres</span>
+                        </div>
+                        <div className="_measurement">
+                            <span>354 </span>
+                            <span>strokes</span>
+                        </div>
+                        <div className="_measurement">
+                            <span>10 </span>
+                            <span>minutes</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="_day">
+                    <h1>Monday, 12th March</h1>
+                    <hr />
+                    <div className="_exercise-group">
+                        <div className="_exercise">
+                            <span className="_type">Bicep Curl</span>
+                            <div className="_muscle _biceps">
+                                <img alt="" />
+                                <span>Biceps</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>10 </span>
+                                <span>reps</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>5 </span>
+                                <span>kgs</span>
+                            </div>
+                        </div>
+                        <div className="_rest _short">
+                            <span>30 seconds rest</span>
+                        </div>
+                        <div className="_exercise">
+                            <span className="_type">Bicep Curl</span>
+                            <div className="_muscle _biceps">
+                                <img alt="" />
+                                <span>Biceps</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>10 </span>
+                                <span>reps</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>5 </span>
+                                <span>kgs</span>
+                            </div>
+                        </div>
+                        <div className="_rest _short">
+                            <span>30 seconds rest</span>
+                        </div>
+                        <div className="_exercise">
+                            <span className="_type">Bicep Curl</span>
+                            <div className="_muscle _biceps">
+                                <img alt="" />
+                                <span>Biceps</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>5 </span>
+                                <span>reps</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>5 </span>
+                                <span>kgs</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="_exercise-group">
+                        <div className="_exercise">
+                            <span className="_type">Pushup</span>
+                            <div className="_muscle _back">
+                                <img alt="" />
+                                <span>Back</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>15 </span>
+                                <span>reps</span>
+                            </div>
+                        </div>
+                        <div className="_rest _medium">
+                            <span>2 minute rest</span>
+                        </div>
+                        <div className="_exercise">
+                            <span className="_type">Pushup</span>
+                            <div className="_muscle _back">
+                                <img alt="" />
+                                <span>Back</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>15 </span>
+                                <span>reps</span>
+                            </div>
+                        </div>
+                        <div className="_rest _none">
+                            <span>No rest</span>
+                        </div>
+                        <div className="_exercise">
+                            <span className="_type">Pushup</span>
+                            <div className="_muscle _back">
+                                <img alt="" />
+                                <span>Back</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>12 </span>
+                                <span>reps</span>
+                            </div>
+                        </div>
+                        <div className="_rest _long">
+                            <span>5 minutes rest</span>
+                        </div>
+                        <div className="_exercise">
+                            <span className="_type">Pushup</span>
+                            <div className="_muscle _back">
+                                <img alt="" />
+                                <span>Back</span>
+                            </div>
+                            <div className="_measurement">
+                                <span>10 </span>
+                                <span>reps</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="_day">
+                    <h1>Wednesday, 14th March</h1>
+                    <hr />
+                    <div className="_exercise">
+                        <span className="_type">Bicep Curl</span>
+                        <div className="_muscle _biceps">
+                            <img alt="" />
+                            <span>Biceps</span>
+                        </div>
+                        <div className="_measurement">
+                            <span>15 </span>
+                            <span>reps</span>
+                        </div>
+                        <div className="_measurement">
+                            <span>10 </span>
+                            <span>kgs</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="exercise-input"></div>
+        </div>
+    );
+}
+
+export default GlobalFeedView;
